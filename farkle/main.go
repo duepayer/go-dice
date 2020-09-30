9b779e7596066fbf2f0c1bd85b056a8fa07bc80c
Shawn Bacot <duepayer@gmail.com>
2020-09-30 14:51:14 -0700

    Support parsing user cli input to set number of dice

--- farkle/main.go
+++ farkle/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/duepayer/go-dice/internal/rolldice"
 )
@@ -10,9 +12,13 @@ func main() {
 	fmt.Println("Start Game")
 	// dice := make([]int, 6)
 	// fmt.Println("dice:", dice)
-
-	roll := rolldice.RollDice(4)
+	numOfDice, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		panic("User Arg Parsing Error")
+	}
+	fmt.Printf("Number of Dice: %v\n", numOfDice)
+	roll := rolldice.RollDice(numOfDice)
 	rc := rolldice.RollCount(roll)
 	fmt.Println(roll)
-	fmt.Println(len(rolldice.ScoreRoll(rc)))
+	fmt.Println(len(rolldice.RollScore(rc)))
 }
