8d34fcf562e640fee3a6860dbc7e980029566ba4
Shawn Bacot <duepayer@gmail.com>
2020-10-01 19:22:38 -0700

    reorganize app structure

--- farkle/farkle.go
+++ farkle/farkle.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/duepayer/go-dice/internal/dice"
+)
+
 /* 	Farkle 5000 Rules
 https://www.dicegamedepot.com/farkle-rules/
 
@@ -24,7 +32,7 @@ Four 6's	1200 points
 To_Do
 [] Learn how to print dice to screen
 [] Create scoring algorithm
-
+[] Support interactive user input
 */
 
 // Farkle - game configuration
@@ -38,13 +46,81 @@ type Farkle struct {
 
 // Player - a place to store player meta
 type Player struct {
-	name  string
-	score Score
-	target
+	name        string
+	score       Score
+	tempScore   Score
+	rollHistory []Score
 }
 
-// Score - all player score related meta
+// Score - all score related meta
 type Score struct {
-	Score     int
-	tempScore int
+	Score int
+	Dice  []int
+}
+
+// RollScore - tally and calculate all possible scoring combinations
+// and return an array of mapped scores
+func RollScore(count map[int]int) []Score {
+	scores := make([]Score, 15)
+
+	// Remove non-scoring die from count map
+	for k, i := range count {
+		if i == 0 {
+			delete(count, k)
+		}
+	}
+
+	if len(count) == 6 {
+		d := []int{1, 2, 3, 4, 5, 6}
+		s := Score{
+			Score: 3000,
+			Dice:  d,
+		}
+		scores = append(scores, s)
+	} else {
+		// Separate counts into singles, pairs, triplets and quadruplets
+		ones := make([]int, 1)
+		fives := make([]int, 1)
+		pairs := make([]int, 3)
+		triplets := make([]int, 2)
+		quadruplets := make([]int, 1)
+		quintuplets := make([]int, 1)
+		sextuplets := make([]int, 1)
+
+		for k, i := range count {
+			switch i {
+			case 1:
+				if k == 1 {
+					ones = append(ones, i)
+				} else if k == 5 {
+					fives = append(fives, i)
+				}
+			case 2:
+				pairs = append(pairs, k)
+			case 3:
+				triplets = append(triplets, k)
+			case 4:
+				quadruplets = append(quadruplets, k)
+			case 5:
+				quintuplets = append(quintuplets, k)
+			case 6:
+				sextuplets = append(sextuplets, k)
+			}
+		}
+	}
+	return scores
+}
+
+func main() {
+	fmt.Println("Start Game")
+
+	numOfDice, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		panic("User Arg Parsing Error")
+	}
+	fmt.Printf("Number of Dice: %v\n", numOfDice)
+	roll := dice.RollDice(numOfDice)
+	rc := dice.RollCount(roll)
+	fmt.Println(roll)
+	fmt.Println(RollScore(rc))
 }
